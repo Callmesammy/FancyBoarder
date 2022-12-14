ae422afa2c26dd030b19f14add187360a4bd3146
HP <callmesammy@yahoo.com>
2022-12-14 12:47:32 +0100

    ghgh

--- src/fancy_boarder/panel.java
+++ src/fancy_boarder/panel.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import javax.swing.JComponent;
-import swings.FancyBorderRadius;;l
+import swings.FancyBorderRadius;
 ss
 
 public class panel extends JComponent{
