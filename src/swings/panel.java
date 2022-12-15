253d97d7565308820b31bb00036f1d984a58e92c
HP <HP@DESKTOP-LLV3NL7>
2022-12-15 13:41:21 +0100

    Sett

--- src/swings/panel.java
+++ src/swings/panel.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package swings;
 
 import java.awt.Color;
@@ -34,7 +31,7 @@ public class panel extends JComponent{
         g2.fill(border.getShape());
         g2.dispose();
         super.paintComponent(g);
-    }tt
+    }
     
     
     
