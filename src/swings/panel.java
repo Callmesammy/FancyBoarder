eb22d7ea9fdafd1d36b6e42c3aa65609cafc211a
HP <callmesammy@yahoo.com>
2023-01-03 11:39:54 +0100

    Settings

--- src/swings/panel.java
+++ src/swings/panel.java
@@ -1,6 +1,8 @@
+
 package swings;
 
 import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,7 +12,6 @@ import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import static java.util.Collections.rotate;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
@@ -18,96 +19,128 @@ import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
 
-/**
- *
- * @author HP
- */
-
-// no comments for now :)
+//Thanks ;))))))))))
 public class panel extends JComponent{
 
-    private Icon image;
-    private float minate;
-    private Animator animate;
-    private Animator amitor;
-    private float mitor;
-    private FancyAnimation fancy;
+    /**
+     * @return the image
+     */
+    public Icon getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(Icon image) {
+        this.image = image;
+        repaint();
+    }
+
+    // we create animation :)
+    private Icon image; 
     
-            // Bye for now :))
+    private Animator animate;
+    private float minate;
+    ccv
+    // next we create a 360 degree rotaton on the image :)
+    
+    private Animator minator;
+    private float min;
+    
+    // next we create a start and stop option ;)
     public panel() {
         image = new ImageIcon(getClass().getResource("/image/pack/picture.jpg"));
         init();
     }
     
     private void init(){
-        String start = "40% 60% 60% 40% / 60% 30% 70% 40%";
-        String end = "40% 60%";
-        fancy = new FancyAnimation(start, end);
         TimingTarget target = new TimingTargetAdapter(){
             @Override
             public void timingEvent(float fraction) {
-                minate = fraction;
-                repaint();
-            }
+               minate = fraction;
+               repaint();
+              }
+            
         
         };
         animate = new Animator(5000, target);
-        animate.setResolution(0);
-// mate.setDecelera       animate.setAcceleration(0.55f);
-//        anition(0.5f);
+        animate.setResolution(5);
         animate.setRepeatCount(-1);
         animate.setRepeatBehavior(Animator.RepeatBehavior.REVERSE);
-        animate.start();
-    
+       
         
-          TimingTarget targt = new TimingTargetAdapter(){
+        
+         TimingTarget targe = new TimingTargetAdapter(){
             @Override
             public void timingEvent(float fraction) {
-                mitor = fraction;
-                repaint();
-            }
+               min = fraction;
+               repaint();
+                         }
+            
         
         };
-        amitor = new Animator(10000, targt);
-        amitor.setResolution(0);
-// mate.setDecelera       animate.setAcceleration(0.55f);
-//        anition(0.5f);
-        amitor.setRepeatCount(-1);
-        amitor.setRepeatBehavior(Animator.RepeatBehavior.REVERSE);
-        amitor.start();
-        
-        
+        minator = new Animator(10000, targe);
+        minator.setResolution(5);
+        minator.setRepeatCount(-1);
+        minator.setRepeatBehavior(Animator.RepeatBehavior.LOOP);
        
+    }
+    
+    public void start(){
+        if (!animate.isRunning()) {
+            animate.start();
+            minator.start();
+            
+        }
+    }
+    public  void stop(){
+        animate.stop();
+        minator.stop();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-    
-        int width = getWidth();
-        int height = getHeight();
-        int x =0;
-        int y =0;
-        FancyBorderRadius fan = new FancyBorderRadius(width, height, fancy.getAnimate(minate));
-        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+     
+        double width = getWidth()*0.8f;
+        double height = getHeight()*0.8f;
+        double x = (getWidth()-width)/2;
+        double y = (getHeight()-height)/2;
+        String start = "40% 60% 60% 40% / 60% 30% 70% 40%";
+        String end = "40% 60%";
+        FancyAnimation ani = new FancyAnimation(start, end);
+        BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = img.createGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        AffineTransform tr = g2.getTransform();
-        g2.rotate(Math.toRadians(360*mitor), width/2, height/2); 
+        FancyBorderRadius fan = new FancyBorderRadius(width, height, ani.getAnimate(minate));
         
+        // for smootness of the lines 
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        // for smootness of the image
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.setColor(new Color(45, 5, 51));
+        // next lets bring the image to same shape with the image ;)
+        
+        AffineTransform tran = g2.getTransform();
+        g2.translate(x, y);
+        g2.rotate(Math.toRadians(360*min), width/2, height/2);
+        
+        
+       
         g2.fill(fan.getShape());
-        if (image != null) {
-            
-            g2.setComposite(AlphaComposite.SrcIn);
-            Rectangle rec = getAutoSize(image);
-            g2.drawImage(toImage(image), rec.x, rec.y, rec.width, rec.height, null);
-            
-        }
+        if (getImage() != null) {
+            g2.setTransform(tran);
+             g2.setComposite(AlphaComposite.SrcIn);
+            Rectangle rec = getAutoSize(getImage());
+            g2.drawImage(toImage(getImage()), rec.x, rec.y, rec.width, rec.height, null);
+        }  g2.dispose();
         g2.dispose();
-        g.drawImage(img, 0,0,null);
+        g.drawImage(img, 0, 0, null);
         super.paintComponent(g); 
-    
     }
     
+    
+        
      private Rectangle getAutoSize(Icon image) {
         int w = getWidth();
         int h = getHeight();
@@ -132,5 +165,6 @@ public class panel extends JComponent{
     private Image toImage(Icon icon) {
         return ((ImageIcon) icon).getImage();
     }
+    
     
 }
